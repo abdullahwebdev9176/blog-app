06f0420baa86039013efd62dbef8fb0eecab8f45
programmer9176 <myeducation9176@gmail.com>
2025-06-28 23:23:50 +0500

    added blog form

--- app/admin/AddBlog/page.jsx
+++ app/admin/AddBlog/page.jsx
@@ -3,18 +3,32 @@
 const page = () => {
   return (
     <div>
-        <h1 className="text-2xl font-bold mb-4">Add New Blog</h1>
-        <form className="space-y-4">
-            <div>
-            <label className="block text-sm font-medium mb-1">Title</label>
-            <input type="text" className="w-full p-2 border rounded" placeholder="Enter blog title" />
-            </div>
-            <div>
-            <label className="block text-sm font-medium mb-1">Content</label>
-            <textarea className="w-full p-2 border rounded" rows="5" placeholder="Write your blog content here"></textarea>
-            </div>
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">Add Blog</button>
-        </form>
+      <h1 className="text-2xl font-bold mb-4">Add New Blog</h1>
+      <form className="space-y-4">
+        <div className="mb-3">
+          <label className="mb-1">Title</label>
+          <input type="text" className="form-control" placeholder="Enter blog title" />
+        </div>
+        <div className="mb-3">
+          <label className="mb-1">Category</label>
+          <input type="text" className="form-control" placeholder="Enter blog Category" />
+        </div>
+        <div className="mb-3">
+          <label className="mb-1">Content</label>
+          <textarea className="form-control" rows="5" placeholder="Write your blog content here"></textarea>
+        </div>
+        <div className="mb-3">
+          <label className="mb-1">Author</label>
+          <input type="text" className="form-control" placeholder="Enter blog Author" />
+        </div>
+
+        <div className="mb-3">
+          <label className="mb-1" htmlFor="">Thumbnail Image</label>
+          <input type="file" id="thumbnail-image" className="form-control" placeholder="Enter blog Author" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Add Blog</button>
+      </form>
     </div>
   )
 }
