0910c6cc3859dbf9ff83afe15039e4effb5b31e9
programmer9176 <myeducation9176@gmail.com>
2025-06-28 20:13:44 +0500

    created blog api

---
+++ app/api/blog/route.js
@@ -0,0 +1,61 @@
+export const runtime = "nodejs";
+import { connectDB } from "@/lib/config/db";
+import { NextResponse } from "next/server";
+import { writeFile } from "fs/promises";
+import { BlogModel } from "@/lib/models/BlogModel";
+
+const LoadDB = async () => {
+    await connectDB();
+};
+
+LoadDB().catch((error) => {
+    console.error("Error connecting to the database:", error);
+});
+
+export async function GET(request) {
+    console.log("GET request received for /api/blog");
+
+    return NextResponse.json({
+        message: "GET request received for /api/blog",
+        method: "GET",
+    });
+}
+
+
+export async function POST(request) {
+
+    const formData = await request.formData();
+
+    const timestamp = Date.now();
+    const image = formData.get("image");
+    if (!image) {
+        return NextResponse.json({ error: "No image uploaded" }, { status: 400 });
+    }
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path, buffer);
+
+    const imageUrl = `/${timestamp}_${image.name}`;
+
+    console.log(imageUrl);
+
+
+    const blogData = {
+        title: formData.get("title"),
+        description: formData.get("description"),
+        image: imageUrl,
+        category: formData.get("category"),
+        auther: formData.get("auther"),
+    };
+
+    await BlogModel.create(blogData);
+
+    return NextResponse.json({
+        succes: true,
+        message: "Blog created successfully",
+        data: blogData
+    });
+
+}
